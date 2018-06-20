5c27315f68dda4b43eea717c05298e6df60e8e0f
zhanqin <1332184878@qq.com>
2018-06-20 14:23:54 +0800

    Initial Commit

---
+++ emoj_demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  emoj_demo
+//
+//  Created by zhanqin on 2018/6/20.
+//  Copyright © 2018年 zhanqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
